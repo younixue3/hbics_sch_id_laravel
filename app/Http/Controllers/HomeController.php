802f3e2d560b368556a82ba2ac20514cd2ddc86d
younixue3 <bussiness.needs.rctka@gmail.com>
2022-02-15 10:31:21 +0800

    Fixing Header

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -14,7 +14,7 @@ class HomeController extends Controller
      */
     public function __construct(DataController $data)
     {
-        $this->middleware('auth');
+//        $this->middleware('auth');
         $this->data = $data;
     }
 
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,7 +11,7 @@
             </div>
         </div>
         <div class="grid grid-cols-1 gap-y-10 bg-white">
-            <div class="px-5">
+            <div class=" px-10 lg:px-80">
                 <div class="m-auto w-full align-middle">
                     <div
                         class="absolute text-gray-300 -translate-x-10 -translate-y-5 md:-translate-x-10 md:-translate-y-10 text-6xl md:text-8xl">
@@ -28,7 +28,7 @@
             </div>
             <div class="md:flex px-5">
                 <div class="md:w-2/5 h-96 bg-white rounded-3xl shadow-lg shadow-neutral-500">
-                    <img class="md:w-[35rem] h-96 rounded-3xl object-cover"
+                    <img class="w-full h-96 rounded-3xl object-cover"
                          src="{{asset('assets/stock_photo_rdev/Pic-1.jpg')}}">
                 </div>
                 <div class="md:w-3/5 grid grid-cols-1 gap-5 text-center md:text-start md:pl-10 py-10">
@@ -42,11 +42,11 @@
             </div>
             <div class="relative flex my-20">
                 <div
-                    class="absolute bottom-52 w-full h-96 rounded-3xl bg-gradient-to-br from-sky-600 to-blue-900 transform scale-95 skew-y-3 z-0 shadow-lg shadow-neutral-500 transition-all ease-in-out duration-500">
+                    class="absolute bottom-44 w-full h-96 rounded-3xl bg-gradient-to-br from-sky-600 to-blue-900 transform scale-95 skew-y-3 z-0 shadow-lg shadow-neutral-500 transition-all ease-in-out duration-500">
 
                 </div>
                 <div class="w-full lg:flex">
-                    <div class="pt-10 pl-10 z-10">
+                    <div class="w-1/4 pt-10 pl-10 z-10 order-first lg:order-last">
                         <h1 class="text-5xl font-bold uppercase">Prestasi Kami</h1>
                     </div>
                     <div class="lg:w-3/4 h-[46rem] grid md:grid-cols-4 md:grid-rows-2 gap-8 overflow-hidden p-5 lg:p-16 lg:pr-0 z-10">
@@ -71,10 +71,10 @@
 
                 </div>
                 <div class="w-full lg:flex">
-                    <div class="pt-10 pl-10 z-10">
+                    <div class="w-1/4 pt-10 pl-10 z-10">
                         <h1 class="text-5xl font-bold uppercase">Alumni Harapan Bangsa</h1>
                     </div>
-                    <div class="lg:w-3/4 h-[46rem] grid md:grid-cols-4 md:grid-rows-2 gap-8 overflow-hidden p-5 lg:p-16 lg:pr-0 z-10">
+                    <div class="lg:w-3/4 h-[46rem] grid md:grid-cols-4 md:grid-rows-2 gap-8 overflow-hidden p-5 lg:p-16 lg:pl-0 z-10">
                         <content-card-component imagecontent="{{asset('assets/stock_photo_rdev/Pic-1.jpg')}}" insight="50"
                                                 title="Lorem Ipsum"
                                                 content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas a mauris ut ipsum varius blandit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sed nisl non ante semper vehicula. Morbi pretium, nisl in egestas iaculis, augue enim scelerisque neque, in scelerisque dolor justo quis sapien. Mauris suscipit eleifend eros non condimentum. Maecenas felis mi, efficitur tincidunt accumsan vel, lacinia sed nisi. Cras cursus elit sed ligula volutpat, id malesuada felis facilisis. Suspendisse mauris quam, scelerisque nec purus eget, pretium volutpat magna. Vivamus tempus sit amet nibh vitae imperdiet. Curabitur congue dignissim consequat. Maecenas quis sapien ut nibh consectetur feugiat id ut arcu. Cras odio nulla, laoreet sed metus a, malesuada dictum dui. Nulla rutrum, ipsum sit amet tristique cursus, ligula dui sagittis quam, a mattis ipsum sapien sit amet est."></content-card-component>
