8a9af15cedc2d528e0f8814332b1c8d90f682450
younixue3 <bussiness.needs.rctka@gmail.com>
2022-03-07 16:13:03 +0800

    Fixing Design

--- resources/views/front/akademis/sma/index.blade.php
+++ resources/views/front/akademis/sma/index.blade.php
@@ -6,7 +6,7 @@
             <div class="m-auto w-full h-full text-center text-white flex relative backdrop-brightness-75">
                 <div class="mx-auto mt-auto mb-20">
                     <h1 class="text-2xl md:text-6xl font-bold uppercase">
-                        Happy Holy Kids Balikpapan
+                        SMA Kristen Harapan Bangsa Balikpapan
                     </h1>
                 </div>
             </div>
@@ -14,23 +14,23 @@
         <div class="bg-pattern">
             <div class="flex">
                 <div class="px-10 py-10 text-center text-lg">
-                    Happy Holy Kids sendiri merupakan satu-satunya di Kalimantan Timur dan Yayasan Tunas Cahaya Bangsa adalah satu-satunya sekolah yang mendapatkan perjanjian eksklusif dengan Happy Holy Kids pusat yang berada di Jakarta. Happy Holy Kids berdiri pada tanggal 17 Juli 2006. Kurikulum Happy Holy Kids menggunakan pendekatan saintifik dengan beragam sentra pembelajaran. Sentra dirancang sebagai tempat dimana anak-anak belajar.  Mereka mengeksplorasi lingkungan mereka melalui proses pembelajaran lewat permainan. Happy Holy Kids juga memiliki program karya wisata di mana anak-anak dapat belajar dunia di sekitar mereka lebih dekat dengan mengalaminya.
+                    SMA Harapan Bangsa sebagai SMA kristen unggulan Balikpapan juga menggunakan kurikulum Hybrid dimana kurikulum Nasional diperkaya dengan kurikulum Cambridge. Selama belajar di tingkat SMA, seluruh peserta didik harus memiliki akses, pengetahuan dan pengalaman dalam belajar lebih banyak dibandingkan tingkat sebelumnya. Pada tingkat ini, siswa/i didorong untuk memiliki kemampuan abad 21. Kemampuan ini untuk membantu siswa/i menghadapi masa depan. Kami mempersiapkan siswa/i untuk memasuki lingkungan kuliah dan kesempatan menjadi tenaga profesional ahli di masa depan. Siswa/i Sekolah Menengah Atas Harapan Bangsa Balikpapan memiliki kesempatan terlibat dalam masyarakat melalui program Live-In di mana mereka dapat mengalami nilai hidup dan menjadi bagian sebagai pemberi solusi yang berdampak bagi masyarakat.
                 </div>
             </div>
             <div class="text-center">
-                <h2 class="text-3xl font-bold">Pembelajaran Happy Holy Kids sebagai TK Terbaik Balikpapan</h2>
+                <h2 class="text-3xl font-bold">Pembelajaran Sekolah Kristen Harapan Bangsa Balikpapan, sebagai SMA Terbaik Balikpapan</h2>
                 <div class="grid grid-cols-1 grid-rows-3 md:grid-cols-2 md:grid-rows-2 gap-5 py-10 px-5 md:px-20">
                     <div class="flex justify-self-end">
                         <div class="bg-white m-auto md:w-96 rounded-xl shadow-xl p-5 transform hover:scale-105 transition-all ease-in-out duration-500">
-                            <h4 class="text-xl font-bold"> Sentra Belajar :</h4>
+                            <h4 class="text-xl font-bold"> Ekstrakurikuler :</h4>
                             <ul>
-                                <li>Sentra Balok</li>
-                                <li>Sentra Bermain Peran</li>
-                                <li>Sentra Persiapan</li>
-                                <li>Sentra Alama</li>
-                                <li>Sentra Seni</li>
-                                <li>Sentra Mulok English</li>
-                                <li>Sentra Mulok Mandarin</li>
+                                <li>Keyboard</li>
+                                <li>Art & Craft</li>
+                                <li>Drawing & Coloring</li>
+                                <li>English</li>
+                                <li>Vocal</li>
+                                <li>Dance</li>
+                                <li>Obstacle</li>
                             </ul>
                         </div>
                     </div>
@@ -49,15 +49,15 @@
                     </div>
                     <div class="md:col-span-2 flex">
                         <div class="bg-white m-auto md:w-96 rounded-xl shadow-xl p-5 transform hover:scale-105 transition-all ease-in-out duration-500">
-                            <h4 class="text-xl font-bold"> Ekstrakurikuler :</h4>
+                            <h4 class="text-xl font-bold"> Sentra Belajar :</h4>
                             <ul>
-                                <li>Keyboard</li>
-                                <li>Art & Craft</li>
-                                <li>Drawing & Coloring</li>
-                                <li>English</li>
-                                <li>Vocal</li>
-                                <li>Dance</li>
-                                <li>Obstacle</li>
+                                <li>Sentra Balok</li>
+                                <li>Sentra Bermain Peran</li>
+                                <li>Sentra Persiapan</li>
+                                <li>Sentra Alama</li>
+                                <li>Sentra Seni</li>
+                                <li>Sentra Mulok English</li>
+                                <li>Sentra Mulok Mandarin</li>
                             </ul>
                         </div>
                     </div>
--- resources/views/front/akademis/smp/index.blade.php
+++ resources/views/front/akademis/smp/index.blade.php
@@ -6,7 +6,7 @@
             <div class="m-auto w-full h-full text-center text-white flex relative backdrop-brightness-75">
                 <div class="mx-auto mt-auto mb-20">
                     <h1 class="text-2xl md:text-6xl font-bold uppercase">
-                        Happy Holy Kids Balikpapan
+                        SMP Kristen Harapan Bangsa Balikpapan
                     </h1>
                 </div>
             </div>
@@ -14,36 +14,35 @@
         <div class="bg-pattern">
             <div class="flex">
                 <div class="px-10 py-10 text-center text-lg">
-                    Happy Holy Kids sendiri merupakan satu-satunya di Kalimantan Timur dan Yayasan Tunas Cahaya Bangsa adalah satu-satunya sekolah yang mendapatkan perjanjian eksklusif dengan Happy Holy Kids pusat yang berada di Jakarta. Happy Holy Kids berdiri pada tanggal 17 Juli 2006. Kurikulum Happy Holy Kids menggunakan pendekatan saintifik dengan beragam sentra pembelajaran. Sentra dirancang sebagai tempat dimana anak-anak belajar.  Mereka mengeksplorasi lingkungan mereka melalui proses pembelajaran lewat permainan. Happy Holy Kids juga memiliki program karya wisata di mana anak-anak dapat belajar dunia di sekitar mereka lebih dekat dengan mengalaminya.
+                    Sekolah Menengah Pertama Harapan Bangsa sebagai SMP Kristen unggulan Balikpapan menggunakan kurikulum Cambridge. Dengan menggunakan kurikulum ini, kami mempersiapkan peserta didik untuk masa depan, membantu mereka mengembangkan keingintahuan mereka, mendorong dan membantu minat mereka yang besar untuk belajar dan menemukan kemampuan baru dan dunia dalam cakupan yang lebih luas. Pada tingkatan ini, peserta didik melanjutkan untuk lebih memaksimalkan potensi mereka melalui karya mereka di berbagai kegiatan seperti Kidspreneurship. Peserta didik dilatih dan dipersiapkan menjadi pemimpin masa depan melalui program OSIS. Mereka diperkenalkan tentang kepemimpinan dan mengenal diri dan potensi diri mereka sendiri lewat Teens Camp.
                 </div>
             </div>
             <div class="text-center">
-                <h2 class="text-3xl font-bold">Pembelajaran Happy Holy Kids sebagai TK Terbaik Balikpapan</h2>
+                <h2 class="text-3xl font-bold">Pembelajaran di Sekolah Kristen Harapan Bangsa Balikpapan, sebagai SMP Terbaik Balikpapan</h2>
                 <div class="grid grid-cols-1 grid-rows-3 md:grid-cols-2 md:grid-rows-2 gap-5 py-10 px-5 md:px-20">
                     <div class="flex justify-self-end">
                         <div class="bg-white m-auto md:w-96 rounded-xl shadow-xl p-5 transform hover:scale-105 transition-all ease-in-out duration-500">
-                            <h4 class="text-xl font-bold"> Sentra Belajar :</h4>
+                            <h4 class="text-xl font-bold"> Mata Pelajaran :</h4>
                             <ul>
-                                <li>Sentra Balok</li>
-                                <li>Sentra Bermain Peran</li>
-                                <li>Sentra Persiapan</li>
-                                <li>Sentra Alama</li>
-                                <li>Sentra Seni</li>
-                                <li>Sentra Mulok English</li>
-                                <li>Sentra Mulok Mandarin</li>
+                                <li>Religion</li>
+                                <li>Citizenship</li>
+                                <li>Bahasa Indonesia</li>
+                                <li>Mathematics</li>
+                                <li>Science</li>
+                                <li>Social Science</li>
+                                <li>English</li>
+                                <li>The Arts</li>
+                                <li>Health and Physical Education</li>
+                                <li>Mandarin</li>
+                                <li>Information and Communication Technology</li>
                             </ul>
                         </div>
                     </div>
                     <div class="flex justify-self-start">
                         <div class="bg-white m-auto md:w-96 rounded-xl shadow-xl p-5 transform hover:scale-105 transition-all ease-in-out duration-500">
-                            <h4 class="text-xl font-bold"> Sentra Belajar :</h4>
-                            <ul><span class="font-bold">Kelompok Bermain</span>
-                                <li>Sesi Pagi 08.00 - 10.30</li>
-                                <li>Sesi Siang 11.00 - 13.30</li>
-                            </ul>
-                            <ul><span class="font-bold">Taman Kanak-kanak</span>
-                                <li>Sesi Pagi 08.00 - 10.30</li>
-                                <li>Sesi Siang 11.00 - 13.30</li>
+                            <h4 class="text-xl font-bold"> Jam Kegiatan Belajar Mengajar :</h4>
+                            <ul>
+                                <li>Senin - Jum'at 08.00 - 10.45</li>
                             </ul>
                         </div>
                     </div>
@@ -51,22 +50,27 @@
                         <div class="bg-white m-auto md:w-96 rounded-xl shadow-xl p-5 transform hover:scale-105 transition-all ease-in-out duration-500">
                             <h4 class="text-xl font-bold"> Ekstrakurikuler :</h4>
                             <ul>
-                                <li>Keyboard</li>
-                                <li>Art & Craft</li>
-                                <li>Drawing & Coloring</li>
-                                <li>English</li>
-                                <li>Vocal</li>
+                                <li>Band</li>
+                                <li>Coding</li>
+                                <li>Cooking</li>
+                                <li>Content Creator</li>
                                 <li>Dance</li>
-                                <li>Obstacle</li>
+                                <li>Graphic Design</li>
+                                <li>Ilustrasi</li>
+                                <li>Merajut</li>
+                                <li>Panahan</li>
+                                <li>Public Speaking</li>
+                                <li>Robotic</li>
                             </ul>
                         </div>
                     </div>
                 </div>
             </div>
             <div class="text-center">
-                <h2 class="text-3xl font-bold">Prestasi Happy Holy Kids sebagai TK Favorit Balikpapan</h2>
+                <h2 class="text-3xl font-bold">Prestasi Sekolah Kristen Harapan Bangsa Balikpapan, sebagai SMP Favorit Balikpapan</h2>
                 <snap-alumni-component></snap-alumni-component>
             </div>
         </div>
     </div>
 @endsection
+
