c6e6f5eedbbe8a2fefb032b69f6912a346a906e9
younixue3 <bussiness.needs.rctka@gmail.com>
2022-03-17 16:01:29 +0800

    Fixing

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserSeeder::class,
             RoleSeeder::class,
+            FasilitasSeeder::class
         ]);
 
         // \App\Models\User::factory(10)->create();
