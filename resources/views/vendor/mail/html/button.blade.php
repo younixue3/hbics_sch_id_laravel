96c00c4a0e8732aa5145581e6472949e3fa83bfa
Ricko Tiaka <78606675+younixue3@users.noreply.github.com>
2022-05-17 11:25:25 +0800

    Fixing

--- resources/views/vendor/mail/html/button.blade.php
+++ resources/views/vendor/mail/html/button.blade.php
@@ -3,7 +3,7 @@
     <div class="h-32 w-full">
         <img class="h-32 w-full object-cover" src="{{ $asset }}" alt="{{ $title }}">
     </div>
-    <div style="margin:auto;width: 100%;height: 100%;">
+    <div style="display:flex;width: 100%;height: 100%;">
         <a href="{{ $url }}" style="margin:auto;" class="m-auto bg-blue-800 button button-red" target="_blank" rel="noopener">{{$slot}}</a>
     </div>
 </div>
