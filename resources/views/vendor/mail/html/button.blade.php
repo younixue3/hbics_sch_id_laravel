b0651d6cdfc90610d35777a28cdd22ebc43cf212
Ricko Tiaka <78606675+younixue3@users.noreply.github.com>
2022-05-17 11:24:18 +0800

    Fixing

--- resources/views/vendor/mail/html/button.blade.php
+++ resources/views/vendor/mail/html/button.blade.php
@@ -1,9 +1,9 @@
 
-<div style="display: flex;position: relative;">
+<div style="">
     <div class="h-32 w-full">
         <img class="h-32 w-full object-cover" src="{{ $asset }}" alt="{{ $title }}">
     </div>
-    <div style="display: flex; width: 100%;height: 100%;position: absolute !important;">
+    <div style="margin:auto;width: 100%;height: 100%;">
         <a href="{{ $url }}" style="margin:auto;" class="m-auto bg-blue-800 button button-red" target="_blank" rel="noopener">{{$slot}}</a>
     </div>
 </div>
