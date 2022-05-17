ff8691003c0b492d126aad9847355fa11ccd3272
Ricko Tiaka <78606675+younixue3@users.noreply.github.com>
2022-05-17 11:09:32 +0800

    Fixing Markdown Email

--- resources/views/vendor/mail/html/button.blade.php
+++ resources/views/vendor/mail/html/button.blade.php
@@ -1,8 +1,12 @@
 
-
-    <div class="flex relative my-4">
-        {{$slot}}
+<div style="display: flex;position: relative;">
+    <div class="h-32 w-full">
+        <img class="h-32 w-full object-cover" src="{{ $asset }}" alt="{{ $title }}">
     </div>
+    <div style="position: absolute; display: flex; width: 100%;height: 100%" class="flex w-full h-full">
+        <a href="{{ $url }}" style="margin:auto;" class="m-auto bg-blue-800 button button-red" target="_blank" rel="noopener">{{$slot}}</a>
+    </div>
+</div>
 {{--    <div>--}}
 
 {{--        <a href="{{ $url }}" class="button button-{{ $color ?? 'primary' }}" target="_blank" rel="noopener">{{ $slot }}</a>--}}
