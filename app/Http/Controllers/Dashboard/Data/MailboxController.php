b077e70c4a966a13f607ac7f003b5443c01ac9a4
Ricko Tiaka <78606675+younixue3@users.noreply.github.com>
2022-05-20 17:10:08 +0800

    Sent Mailbox

--- app/Http/Controllers/Dashboard/Data/MailboxController.php
+++ app/Http/Controllers/Dashboard/Data/MailboxController.php
@@ -7,5 +7,15 @@ use Illuminate\Http\Request;
 
 class MailboxController extends Controller
 {
-    //
+    public function get_data()
+    {
+        $mailbox = Komunitas::where('connection', $data)->paginate(30);
+        $compact = compact('mailbox');
+        return $compact;
+    }
+
+    public function sent_mail()
+    {
+
+    }
 }
