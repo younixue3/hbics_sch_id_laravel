41b2e59ef7a383dde81447a5e2e340e583ad106a
Ricko Tiaka <78606675+younixue3@users.noreply.github.com>
2022-05-23 12:00:56 +0800

    Fixing

--- database/seeders/KunjungansSeeder.php
+++ database/seeders/KunjungansSeeder.php
@@ -15,7 +15,8 @@ class KunjungansSeeder extends Seeder
     public function run()
     {
         DB::table('kunjungans')->insert([
-            ['name' => 'Ricko', 'email' => 'caesarsmpn21@gmail.com', 'alamat' => 'jl.dimanaaja', 'kota_kab' => 'Samarinda', 'negara' => 'Indonesia', 'telepon' => '081080810', 'nama_calon_siswa' => 'Ricko', 'area_kunjungan' => 'HHK', 'tinkat_pendidikan' => 'HHK', 'tanggal_kunjungan' => ''],
+            ['name' => 'Ricko', 'email' => 'caesarsmpn21@gmail.com', 'alamat' => 'jl.dimanaaja', 'kota_kab' => 'Samarinda', 'negara' => 'Indonesia', 'telepon' => '081080810', 'nama_calon_siswa' => 'Ricko', 'area_kunjungan' => 'HHK', 'tinkat_pendidikan' => 'HHK', 'tanggal_kunjungan' => '2022-05-23 11:58:49'],
+            ['name' => 'Ricko', 'email' => 'caesarsmpn21@gmail.com', 'alamat' => 'jl.dimanaaja', 'kota_kab' => 'Samarinda', 'negara' => 'Indonesia', 'telepon' => '081080810', 'nama_calon_siswa' => 'Ricko', 'area_kunjungan' => 'HHK', 'tinkat_pendidikan' => 'HHK', 'tanggal_kunjungan' => '2022-05-23 11:58:49'],
         ]);
     }
 }
