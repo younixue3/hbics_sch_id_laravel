a23bf3d31e88b69405bbc40a6f7946129954965f
Younixue3 <bussiness.needs.rctka@gmail.com>
2022-11-18 15:58:26 +0800

    Fixing

--- resources/views/front/tentangKami/sejarah/index.blade.php
+++ resources/views/front/tentangKami/sejarah/index.blade.php
@@ -5,7 +5,7 @@
         <h1 id="hero_section" class="mb-20 text-5xl font-bold text-blue-900">Sejarah</h1>
         <div class="px-20">
             <div class="p-10 bg-white rounded-xl shadow-xl">
-                <workspace-pages-output-function editmode="true" fetchdata="{{$pages != null ? $pages->content : ''}}" urlasset="{{asset('Upload/foto_content')}}"></workspace-pages-output-function>
+                <workspace-pages-output-function editmode="true" fetchdata="{{$pages != null ? $pages->item : ''}}" urlasset="{{asset('Upload/foto_content')}}"></workspace-pages-output-function>
             </div>
         </div>
     </div>
